c8386322db6609b6b17c12aa8d98e8c09dd14ce7
twmiles <75818815+twmiles@users.noreply.github.com>
2023-02-07 00:28:53 -0500

    fix: api

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
 module.exports = {
   images: {
     domains: [
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,22 +1,55 @@
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({
+  auth: process.env.NOTION_API_TOKEN,
+});
+
+export default async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ msg: 'Only POST requests are allowed' });
   }
-  return await fn(req, res)
-}
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
+  try {
+    const { name, email, subject, message } = JSON.parse(req.body);
+    await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_DATABASE_ID,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: name,
+              },
+            },
+          ],
+        },
+        Email: {
+          email,
+        },
+        Subject: {
+          rich_text: [
+            {
+              text: {
+                content: subject,
+              },
+            },
+          ],
+        },
+        Message: {
+          rich_text: [
+            {
+              text: {
+                content: message,
+              },
+            },
+          ],
+        },
+      },
+    });
+    res.status(201).json({ msg: 'Success' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: 'Failed' });
+  }
+};
